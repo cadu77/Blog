f978477e80a70c2e263f46e8b073cc02e6e08827
Eduardo Ferreira de Jesus <eduardo-1997@outlook.com.br>
2020-05-17 14:29:12 -0300

    Trabalhando com tamplate

--- blog/website/views.py
+++ blog/website/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def hello_blog(request):
+    lista = ['Django', 'Python', 'Html', 'Banco de dados']
+    data = {'name': 'Django 3', 'lista_tecnologias': lista}
+    return render(request, 'index.html', data)
